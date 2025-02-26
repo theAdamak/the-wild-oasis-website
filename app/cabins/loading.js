e65fc05b8faf9ca71811e6263ef166d4a2a32588
mahdi ezati <mahdiezati80@gmail.com>
2025-02-26 21:30:29 +0330

    added cabins with some new featur.s

---
+++ app/cabins/loading.js
@@ -0,0 +1,12 @@
+import Spinner from "@/app/_components/Spinner";
+
+export default function Loading() {
+  return (
+    <div className=" grid items-center justify-center">
+      <Spinner />
+      <p className=" text-xl text-primary-200 font-semibold">
+        Loading cabin data...
+      </p>
+    </div>
+  );
+}
