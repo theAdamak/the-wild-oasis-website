701da7bc4febbca8ef1648a6568cceeb8e70f680
mahdi ezati <mahdiezati80@gmail.com>
2025-03-08 23:08:31 +0330

    feat: dynamic filter added to cabins.

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -2,15 +2,30 @@ import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
 // import { unstable_noStore as noStore } from "next/cache";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   // only this component will be dynamic
   // noStore();
   const cabins = await getCabins();
 
   if (cabins.length === 0) return null;
+
+  let displayCabin;
+  if (filter === "all") displayCabin = cabins;
+  if (filter === "small") {
+    displayCabin = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  }
+  if (filter === "medium") {
+    displayCabin = cabins.filter(
+      (cabin) => cabin.maxCapacity > 3 && cabin.maxCapacity < 8
+    );
+  }
+  if (filter === "large") {
+    displayCabin = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+  }
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayCabin.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "../_components/Filter";
 
 //for rerender when data change in any time (isr)
 export const revalidate = 3600;
@@ -9,9 +10,10 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+export default async function Page({ serachParams }) {
   // CHANGE
   // suspence must be used in async component!
+  const filter = serachParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -26,8 +28,11 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className=" flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
